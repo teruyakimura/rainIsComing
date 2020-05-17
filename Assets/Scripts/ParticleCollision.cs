71687322543b0dc5b566b27ad183960ad28aabe9
lauracest <laura.cestavo@hotmail.com>
2020-05-17 14:39:02 -0300

    teste de score

--- Assets/Scripts/ParticleCollision.cs
+++ Assets/Scripts/ParticleCollision.cs
@@ -10,11 +10,13 @@ public class ParticleCollision : MonoBehaviour
     public Button Quit_button;
     public ParticleSystem RainGenerator;
     public PlayerMovement playerMove;
-    
+    public Text score;
+    public Score score_code;
     private void OnParticleCollision(GameObject other)
     {
         if (other.layer.Equals(9)) {
             playerMove.GetComponent<PlayerMovement>();
+            score.text = "";
             GameOver_text.text = "GAME OVER";
             Restart_button.gameObject.SetActive(true);
             Quit_button.gameObject.SetActive(true);
@@ -22,6 +24,9 @@ public class ParticleCollision : MonoBehaviour
             playerMove.runSpeed = 0;
             //yield return new WaitForSeconds(4);
             playerMove.able = false;
+            score_code.enable_score = false;
+
+
         }
     }
 
